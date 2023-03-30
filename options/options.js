e42d899efc944764d955bf9a95f5f429762a8370
Crowdbotics <engineering@crowdbotics.com>
2023-03-30 00:15:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-sky-dev-73056",
+  url: "https://lucky-sky-dev-73056.botics.co",
+  api: "https://lucky-sky-dev-73056.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
